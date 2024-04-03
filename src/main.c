bf615ce859796ec00baf2660f789e1c4fdb95a86
e2dk4r <43293320+e2dk4r@users.noreply.github.com>
2024-04-03 13:10:56 +0300

    src: fix multiple gamepad events not detected
    When 2 gamepads connected, if events from each read, app would shows
    garbage. This commit fixes that.
    
    Add manual memory management system for allocating events on the fly.
    memory_block is used for all the heap storage that app can use.
    memory_chunk is used for dividing our heap storage. memory_chunk not
    deallocates memory_block ever, it just flags it for empty. Next push()
    is gives back first empty location.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #define _DEFAULT_SOURCE
 #define _XOPEN_SOURCE 700
 
+#include <assert.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <libevdev/libevdev.h>
@@ -9,6 +10,7 @@
 #include <stdio.h>
 #include <sys/inotify.h>
 #include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 #define POLLIN 0x001  /* There is data to read.  */
@@ -32,6 +34,8 @@ typedef unsigned long long u64;
 #define GAMEPAD_ERROR_IO_URING_SETUP 1
 #define GAMEPAD_ERROR_IO_URING_WAIT 2
 
+#define GAMEPAD_ERROR_MEMORY 40
+
 #define GAMEPAD_ERROR_INOTIFY_SETUP 10
 #define GAMEPAD_ERROR_INOTIFY_WATCH_SETUP 11
 #define GAMEPAD_ERROR_INOTIFY_WATCH 12
@@ -43,6 +47,7 @@ typedef unsigned long long u64;
 #define GAMEPAD_ERROR_LIBEVDEV 30
 #define GAMEPAD_ERROR_LIBEVDEV_FD 30
 
+#define debug(str) write(2, "d: " str, 3 + sizeof(str) - 1)
 #define fatal(str) write(2, "e: " str, 3 + sizeof(str) - 1)
 #define warning(str) write(2, "w: " str, 3 + sizeof(str) - 1)
 
@@ -53,16 +58,76 @@ struct op {
 
 struct op_device_open {
   u8 type;
-  const char *path;
+  const char path[32];
 };
 
 struct op_joystick_read {
   u8 type;
   u8 initialized : 1;
-  u16 connectedAt;
   int fd;
   struct input_event event;
 };
+
+struct memory_block {
+  void *block;
+  u64 used;
+  u64 total;
+};
+
+struct memory_chunk {
+  void *block;
+  u64 size;
+  u64 max;
+};
+
+#define KILOBYTES (1 << 10)
+#define MEGABYTES (1 << 20)
+#define GIGABYTES (1 << 30)
+
+static void *mem_chunk_push(struct memory_chunk *chunk) {
+  debug("mem_chunk_push\n");
+  void *result = 0;
+  void *dataBlock = chunk->block + sizeof(u8) * chunk->max;
+  for (u64 index = 0; index < chunk->max; index++) {
+    u8 *flag = chunk->block + sizeof(u8) * index;
+    if (*flag == 0) {
+      result = dataBlock + index * chunk->size;
+      *flag = 1;
+      return result;
+    }
+  }
+
+  return result;
+}
+
+static void mem_chunk_pop(struct memory_chunk *chunk, void *block) {
+  debug("mem_chunk_pop\n");
+  void *dataBlock = chunk->block + sizeof(u8) * chunk->max;
+  u64 index = (block - dataBlock) / chunk->size;
+  u8 *flag = chunk->block + sizeof(u8) * index;
+  *flag = 0;
+}
+
+static void *mem_push(struct memory_block *mem, u64 size) {
+  assert(mem->used + size <= mem->total);
+  void *result = mem->block + mem->used;
+  mem->used += size;
+  return result;
+}
+
+static struct memory_chunk *mem_push_chunk(struct memory_block *mem, u64 size,
+                                           u64 max) {
+  struct memory_chunk *chunk =
+      mem_push(mem, sizeof(*chunk) + max * sizeof(u8) + max * size);
+  chunk->block = chunk + sizeof(*chunk);
+  chunk->size = size;
+  chunk->max = max;
+  for (u64 index = 0; index < chunk->max; index++) {
+    u8 *flag = chunk->block + sizeof(u8) * index;
+    *flag = 0;
+  }
+  return chunk;
+}
 
 static inline u8 libevdev_is_joystick(struct libevdev *evdev) {
   return libevdev_has_event_type(evdev, EV_ABS) &&
@@ -77,6 +142,26 @@ int main(void) {
     error_code = GAMEPAD_ERROR_IO_URING_SETUP;
     goto exit;
   }
+
+  /* memory */
+  struct memory_block memory_block = {};
+  memory_block.total = 256 * KILOBYTES;
+  memory_block.block =
+      mmap(0, (size_t)memory_block.total, PROT_READ | PROT_WRITE,
+           MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (!memory_block.block) {
+    fatal("you do not have 256k memory available.\n");
+    error_code = GAMEPAD_ERROR_MEMORY;
+    goto io_uring_exit;
+  }
+
+  struct memory_chunk *MemoryForEvents =
+      mem_push_chunk(&memory_block, sizeof(struct op), 40);
+  struct memory_chunk *MemoryForDeviceOpenEvents =
+      mem_push_chunk(&memory_block, sizeof(struct op_device_open), 10);
+  struct memory_chunk *MemoryForJoystickReadEvents =
+      mem_push_chunk(&memory_block, sizeof(struct op_joystick_read), 10);
+  printf("total memory usage: %llu\n", memory_block.used);
 
   /* notify when a new input added */
   int fd_inotify = inotify_init1(IN_NONBLOCK);
@@ -93,7 +178,9 @@ int main(void) {
   }
 
   struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-  struct op *op = &(struct op){.type = OP_INOTIFY_WATCH, .fd = fd_inotify};
+  struct op *op = mem_chunk_push(MemoryForEvents);
+  op->type = OP_INOTIFY_WATCH;
+  op->fd = fd_inotify;
   io_uring_prep_poll_multishot(sqe, op->fd, POLLIN);
   io_uring_sqe_set_data(sqe, op);
 
@@ -131,19 +218,18 @@ int main(void) {
       *dest = *src;
     }
 
-    struct op_joystick_read *op = &(struct op_joystick_read){
-        .type = OP_JOYSTICK_READ,
-    };
+    struct op_joystick_read stagedOp = {};
+    stagedOp.type = OP_JOYSTICK_READ,
 
-    op->fd = open(path, O_RDONLY | O_NONBLOCK);
-    if (op->fd < 0)
+    stagedOp.fd = open(path, O_RDONLY | O_NONBLOCK);
+    if (stagedOp.fd < 0)
       continue;
 
     struct libevdev *evdev;
-    int rc = libevdev_new_from_fd(op->fd, &evdev);
+    int rc = libevdev_new_from_fd(stagedOp.fd, &evdev);
     if (rc < 0) {
       warning("libevdev failed\n");
-      close(op->fd);
+      close(stagedOp.fd);
       if (evdev)
         libevdev_free(evdev);
       continue;
@@ -151,7 +237,7 @@ int main(void) {
 
     /* detect joystick */
     if (!libevdev_is_joystick(evdev)) {
-      close(op->fd);
+      close(stagedOp.fd);
       libevdev_free(evdev);
       continue;
     }
@@ -161,9 +247,13 @@ int main(void) {
            libevdev_get_id_bustype(evdev), libevdev_get_id_vendor(evdev),
            libevdev_get_id_product(evdev));
 
+    struct op_joystick_read *submitOp =
+        mem_chunk_push(MemoryForJoystickReadEvents);
+    *submitOp = stagedOp;
     struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-    io_uring_prep_read(sqe, op->fd, &op->event, sizeof(op->event), 0);
-    io_uring_sqe_set_data(sqe, op);
+    io_uring_prep_read(sqe, submitOp->fd, &submitOp->event,
+                       sizeof(submitOp->event), 0);
+    io_uring_sqe_set_data(sqe, submitOp);
 
     libevdev_free(evdev);
   }
@@ -239,10 +329,12 @@ int main(void) {
       if (event->mask & IN_DELETE)
         goto cqe_seen;
 
-      struct op_device_open *op = &(struct op_device_open){
-          .type = OP_DEVICE_OPEN,
-          .path = path,
-      };
+      struct op_device_open *submitOp =
+          mem_chunk_push(MemoryForDeviceOpenEvents);
+      submitOp->type = OP_DEVICE_OPEN;
+      for (char *dest = (char *)submitOp->path, *src = path; *src;
+           src++, dest++)
+        *dest = *src;
 
       /* wait for device initialization */
       sqe = io_uring_get_sqe(&ring);
@@ -250,29 +342,29 @@ int main(void) {
           .tv_nsec = 75000000, /* 750ms */
       };
       io_uring_prep_timeout(sqe, ts, 1, 0);
-      io_uring_sqe_set_data(sqe, op);
+      io_uring_sqe_set_data(sqe, submitOp);
       io_uring_submit(&ring);
     }
 
     else if (op->type & OP_DEVICE_OPEN) {
       if (cqe->res < 0 && cqe->res != -ETIME) {
         warning("waiting for device initialiation failed\n");
+        mem_chunk_pop(MemoryForDeviceOpenEvents, op);
         goto cqe_seen;
       }
-      struct op_device_open *currentOp = (struct op_device_open *)op;
 
-      struct op_joystick_read *op = &(struct op_joystick_read){
-          .type = OP_JOYSTICK_READ,
-      };
-
-      op->fd = open(currentOp->path, O_RDONLY | O_NONBLOCK);
-      if (op->fd < 0) {
+      struct op_device_open *op = io_uring_cqe_get_data(cqe);
+      struct op_joystick_read stagedOp = {};
+      stagedOp.type = OP_JOYSTICK_READ;
+      stagedOp.fd = open(op->path, O_RDONLY | O_NONBLOCK);
+      mem_chunk_pop(MemoryForDeviceOpenEvents, op);
+      if (stagedOp.fd < 0) {
         warning("opening device failed\n");
         goto cqe_seen;
       }
 
       struct libevdev *evdev;
-      int rc = libevdev_new_from_fd(op->fd, &evdev);
+      int rc = libevdev_new_from_fd(stagedOp.fd, &evdev);
       if (rc < 0) {
         warning("libevdev failed\n");
         goto error;
@@ -289,9 +381,13 @@ int main(void) {
              libevdev_get_id_bustype(evdev), libevdev_get_id_vendor(evdev),
              libevdev_get_id_product(evdev));
 
+      struct op_joystick_read *submitOp =
+          mem_chunk_push(MemoryForJoystickReadEvents);
+      *submitOp = stagedOp;
       sqe = io_uring_get_sqe(&ring);
-      io_uring_prep_read(sqe, op->fd, &op->event, sizeof(op->event), 0);
-      io_uring_sqe_set_data(sqe, op);
+      io_uring_prep_read(sqe, submitOp->fd, &submitOp->event,
+                         sizeof(submitOp->event), 0);
+      io_uring_sqe_set_data(sqe, submitOp);
       io_uring_submit(&ring);
 
       libevdev_free(evdev);
@@ -301,7 +397,7 @@ int main(void) {
       if (evdev)
         libevdev_free(evdev);
       sqe = io_uring_get_sqe(&ring);
-      io_uring_prep_close(sqe, op->fd);
+      io_uring_prep_close(sqe, stagedOp.fd);
       io_uring_sqe_set_data(sqe, 0);
       io_uring_submit(&ring);
     }
@@ -311,18 +407,19 @@ int main(void) {
       struct input_event *event = &op->event;
 
       /* on joystick read error (eg. joystick removed), close the fd */
-      if (cqe->res < 0) {
+      if (cqe->res < 0 && cqe->res != -EAGAIN) {
         warning("cannot read events from device. maybe disconnected?\n");
         sqe = io_uring_get_sqe(&ring);
         io_uring_prep_close(sqe, op->fd);
         io_uring_sqe_set_data(sqe, 0);
         io_uring_submit(&ring);
+        mem_chunk_pop(MemoryForJoystickReadEvents, op);
         goto cqe_seen;
       }
 
-      printf("fd: %d time: %ld.%ld type: %d code: %d value: %d\n", op->fd,
-             event->input_event_sec, event->input_event_usec, event->type,
-             event->code, event->value);
+      printf("%p fd: %d time: %ld.%ld type: %d code: %d value: %d\n", op,
+             op->fd, event->input_event_sec, event->input_event_usec,
+             event->type, event->code, event->value);
 
       /* read event again from gamepad device */
       sqe = io_uring_get_sqe(&ring);
